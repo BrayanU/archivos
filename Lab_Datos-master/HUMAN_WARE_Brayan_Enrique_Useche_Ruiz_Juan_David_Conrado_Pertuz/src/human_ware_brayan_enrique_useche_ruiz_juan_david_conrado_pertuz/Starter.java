48da70e190989d4d6ec6273ec8ccd09b02503599
BrayanU <buseche@uninorte.edu.co>
2018-03-08 22:19:59 -0500

    Add files via upload

---
+++ Lab_Datos-master/HUMAN_WARE_Brayan_Enrique_Useche_Ruiz_Juan_David_Conrado_Pertuz/src/human_ware_brayan_enrique_useche_ruiz_juan_david_conrado_pertuz/Starter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package human_ware_brayan_enrique_useche_ruiz_juan_david_conrado_pertuz;
+
+/**
+ *
+ * @author user
+ */
+public class Starter {
+
+    public static void main(String[] args) {
+        Login x = new Login();
+        x.setVisible(true);
+    }
+}
